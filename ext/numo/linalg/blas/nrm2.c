bc300bfd6f4432269af414c90f8be3f5ef74e988
yoshoku <yoshoku@outlook.com>
2025-09-19 11:25:08 +0900

    refactor: rewrite nrm2 fuctions in C

---
+++ ext/numo/linalg/blas/nrm2.c
@@ -0,0 +1,67 @@
+#include "nrm2.h"
+
+#define DEF_LINALG_FUNC(tElType, tRtType, tNArrType, tRetNArrType, fBlasFnc)     \
+  static void _iter_##fBlasFnc(na_loop_t* const lp) {                            \
+    tElType* x = (tElType*)NDL_PTR(lp, 0);                                       \
+    tRtType* d = (tRtType*)NDL_PTR(lp, 1);                                       \
+    const size_t n = NDL_SHAPE(lp, 0)[0];                                        \
+    tRtType ret = cblas_##fBlasFnc(n, x, 1);                                     \
+    *d = ret;                                                                    \
+  }                                                                              \
+                                                                                 \
+  static VALUE _linalg_blas_##fBlasFnc(int argc, VALUE* argv, VALUE self) {      \
+    VALUE x = Qnil;                                                              \
+    VALUE kw_args = Qnil;                                                        \
+    rb_scan_args(argc, argv, "1:", &x, &kw_args);                                \
+                                                                                 \
+    ID kw_table[1] = { rb_intern("keepdims") };                                  \
+    VALUE kw_values[1] = { Qundef };                                             \
+    rb_get_kwargs(kw_args, kw_table, 0, 1, kw_values);                           \
+    const bool keepdims = kw_values[0] != Qundef ? RTEST(kw_values[0]) : false;  \
+                                                                                 \
+    if (CLASS_OF(x) != tNArrType) {                                              \
+      x = rb_funcall(tNArrType, rb_intern("cast"), 1, x);                        \
+    }                                                                            \
+    if (!RTEST(nary_check_contiguous(x))) {                                      \
+      x = nary_dup(x);                                                           \
+    }                                                                            \
+                                                                                 \
+    narray_t* x_nary = NULL;                                                     \
+    GetNArray(x, x_nary);                                                        \
+                                                                                 \
+    if (NA_NDIM(x_nary) != 1) {                                                  \
+      rb_raise(rb_eArgError, "x must be 1-dimensional");                         \
+      return Qnil;                                                               \
+    }                                                                            \
+    if (NA_SIZE(x_nary) == 0) {                                                  \
+      rb_raise(rb_eArgError, "x must not be empty");                             \
+      return Qnil;                                                               \
+    }                                                                            \
+                                                                                 \
+    ndfunc_arg_in_t ain[1] = { { tNArrType, 1 } };                               \
+    size_t shape_out[1] = { 1 };                                                 \
+    ndfunc_arg_out_t aout[1] = { { tRetNArrType, 0, shape_out } };               \
+    ndfunc_t ndf = { _iter_##fBlasFnc, NO_LOOP | NDF_EXTRACT, 1, 1, ain, aout }; \
+    if (keepdims) {                                                              \
+      ndf.flag |= NDF_KEEP_DIM;                                                  \
+    }                                                                            \
+                                                                                 \
+    VALUE ret = na_ndloop(&ndf, 1, x);                                           \
+                                                                                 \
+    RB_GC_GUARD(x);                                                              \
+    return ret;                                                                  \
+  }
+
+DEF_LINALG_FUNC(double, double, numo_cDFloat, numo_cDFloat, dnrm2)
+DEF_LINALG_FUNC(float, float, numo_cSFloat, numo_cSFloat, snrm2)
+DEF_LINALG_FUNC(dcomplex, double, numo_cDComplex, numo_cDFloat, dznrm2)
+DEF_LINALG_FUNC(scomplex, float, numo_cSComplex, numo_cSFloat, scnrm2)
+
+#undef DEF_LINALG_FUNC
+
+void define_linalg_blas_nrm2(VALUE mBlas) {
+  rb_define_module_function(mBlas, "dnrm2", RUBY_METHOD_FUNC(_linalg_blas_dnrm2), -1);
+  rb_define_module_function(mBlas, "snrm2", RUBY_METHOD_FUNC(_linalg_blas_snrm2), -1);
+  rb_define_module_function(mBlas, "dznrm2", RUBY_METHOD_FUNC(_linalg_blas_dznrm2), -1);
+  rb_define_module_function(mBlas, "scnrm2", RUBY_METHOD_FUNC(_linalg_blas_scnrm2), -1);
+}
---
+++ ext/numo/linalg/blas/nrm2.h
@@ -0,0 +1,13 @@
+#ifndef NUMO_LINALG_ALT_BLAS_NRM2_H
+#define NUMO_LINALG_ALT_BLAS_NRM2_H 1
+
+#include <cblas.h>
+
+#include <ruby.h>
+
+#include <numo/narray.h>
+#include <numo/template.h>
+
+void define_linalg_blas_nrm2(VALUE mBlas);
+
+#endif /* NUMO_LINALG_ALT_BLAS_NRM2_H */
