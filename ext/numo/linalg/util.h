2dd451c8fb04e1c23ba3bd9b263b0ecffbff73ad
yoshoku <yoshoku@outlook.com>
2025-09-19 12:52:19 +0900

    refactor: rewrite util fuctions in C

---
+++ ext/numo/linalg/util.h
@@ -0,0 +1,17 @@
+#ifndef NUMO_LINALG_ALT_UTIL_H
+#define NUMO_LINALG_ALT_UTIL_H 1
+
+#include <cblas.h>
+#include <lapacke.h>
+
+#include <ruby.h>
+
+lapack_int get_itype(VALUE val);
+char get_jobz(VALUE val);
+char get_range(VALUE val);
+char get_uplo(VALUE val);
+int get_matrix_layout(VALUE val);
+enum CBLAS_TRANSPOSE get_cblas_trans(VALUE val);
+enum CBLAS_ORDER get_cblas_order(VALUE val);
+
+#endif // NUMO_LINALG_ALT_UTIL_H
