b39eb70d872615a212f07dabd2b38d4ecb6e3c06
yoshoku <yoshoku@outlook.com>
2025-09-19 11:24:21 +0900

    refactor: rewrite dot fuctions in C

---
+++ ext/numo/linalg/blas/dot.h
@@ -0,0 +1,13 @@
+#ifndef NUMO_LINALG_ALT_BLAS_DOT_H
+#define NUMO_LINALG_ALT_BLAS_DOT_H 1
+
+#include <cblas.h>
+
+#include <ruby.h>
+
+#include <numo/narray.h>
+#include <numo/template.h>
+
+void define_linalg_blas_dot(VALUE mBlas);
+
+#endif /* NUMO_LINALG_ALT_BLAS_DOT_H */
