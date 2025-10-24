081fd63753cb01b54213497686005b98c138dc67
yoshoku <yoshoku@outlook.com>
2025-10-24 16:22:32 +0900

    feat: add accessors for complex types

--- ext/numo/linalg/util.h
+++ ext/numo/linalg/util.h
@@ -6,6 +6,19 @@
 #include <cblas.h>
 #include <lapacke.h>
 
+#ifndef lapack_complex_float_real
+#define lapack_complex_float_real(z) ((z).real)
+#endif
+#ifndef lapack_complex_float_imag
+#define lapack_complex_float_imag(z) ((z).imag)
+#endif
+#ifndef lapack_complex_double_real
+#define lapack_complex_double_real(z) ((z).real)
+#endif
+#ifndef lapack_complex_double_imag
+#define lapack_complex_double_imag(z) ((z).imag)
+#endif
+
 lapack_int get_itype(VALUE val);
 char get_jobz(VALUE val);
 char get_jobvs(VALUE val);
