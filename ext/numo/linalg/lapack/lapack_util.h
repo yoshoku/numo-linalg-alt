4684558e3a7bcab0505e5f9a3d4cc28b63dff4f6
yoshoku <yoshoku@outlook.com>
2025-10-28 16:28:36 +0900

    feat: add accessors for complex types of LAPACK

--- ext/numo/linalg/lapack/lapack_util.h
+++ ext/numo/linalg/lapack/lapack_util.h
@@ -5,6 +5,19 @@
 
 #include <lapacke.h>
 
+#ifndef lapack_complex_float_real
+#define lapack_complex_float_real(z) ((z).real)
+#endif
+#ifndef lapack_complex_float_imag
+#define lapack_complex_float_imag(z) ((z).imag)
+#endif
+#ifndef lapack_complex_double_real
+#define lapack_complex_double_real(z) ((z).real)
+#endif
+#ifndef lapack_complex_double_imag
+#define lapack_complex_double_imag(z) ((z).imag)
+#endif
+
 lapack_int get_itype(VALUE val);
 char get_job(VALUE val, const char* param_name);
 char get_range(VALUE val);
